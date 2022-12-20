5b28b5fdc766c1a0d343bf51c608e5b72bcb250a
Awaid Yasin <awaidyasin@mathesis-vm.us-central1-a.c.ma-thesis-372221.internal>
2022-12-20 22:33:43 +0000

    Latest files

--- install_env.sh
+++ install_env.sh
@@ -1,10 +1,4 @@
-# install miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.10-Linux-x86_64.sh -O ~/miniconda.sh && \
-sudo /bin/bash ~/miniconda.sh -u -b -p /opt/conda && \
-rm ~/miniconda.sh && \
-sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
-source ~/.bashrc
+#code for installing an environment using a yml file
 
 # create conda environment as cant install packages into base
 conda env create -f mathesis_localenv.yml && \
@@ -13,4 +7,6 @@ python -m ipykernel install --user --name mathesis_local --display-name "Python 
 echo "conda activate mathesis_local" > .bashrc
 
 #allow widgets to be displayed
+
 jupyter nbextension enable --py widgetsnbextension
+
